452e96e00b8d138b1d46ee4ae786f9bc58c06af6
lwd3737 <lwd432@gmail.com>
2025-04-10 00:07:55 +0900

    로그인 페이지 디자인 수정

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -3,11 +3,18 @@ import GoogleLoginButton from "./components/GoogleLoginButton";
 
 export default function LoginPage() {
 	return (
-		<main className="flex flex-col items-center gap-y-5 p-14 h-full">
-			<Logo />
+		<main className="flex flex-col justify-center items-center gap-y-5 p-14 h-full">
+			<div className="flex flex-col justify-between items-center bg-white shadow-md p-8 pt-8 pb-12 rounded-lg w-[30vw] h-[60vh]">
+				<div className="text-center">
+					<div className="mb-12">
+						<Logo />
+					</div>
+					<h1 className="mb-2 font-bold text-[#111827] text-[1.875rem]">
+						시작하기
+					</h1>
+					<p className="text-[#6B7280]">계정에 로그인하세요</p>
+				</div>
 
-			<div className="flex flex-col items-center gap-y-8 bg-white shadow-sm p-8 rounded-lg w-[30vw] h-[60vh]">
-				<h1 className="font-bold text-2xl">로그인</h1>
 				<GoogleLoginButton />
 			</div>
 		</main>
