d55c0bb7592a672fe0fefc5dd786a15ab3e37626
lwd3737 <lwd432@gmail.com>
2025-08-24 21:02:51 +0900

    Add: migration

---
+++ prisma/migrations/20250824115722_init/migration.sql
@@ -0,0 +1,102 @@
+-- CreateEnum
+CREATE TYPE "OauthProviderType" AS ENUM ('GOOGLE');
+
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN');
+
+-- CreateEnum
+CREATE TYPE "ContentType" AS ENUM ('YOUTUBE_VIDEO');
+
+-- CreateEnum
+CREATE TYPE "VerdictType" AS ENUM ('TRUE', 'MOSTLY_TRUE', 'MIXED', 'MOSTLY_FALSE', 'FALSE', 'UNKNOWN');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "provider" "OauthProviderType" NOT NULL,
+    "providerSub" TEXT,
+    "email" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'USER',
+    "refreshToken" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "YoutubeVideo" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "channelId" TEXT NOT NULL,
+    "channelTitle" TEXT NOT NULL,
+    "thumbnailUrl" TEXT NOT NULL,
+    "transcript" TEXT,
+    "transcriptMetadata" JSONB,
+    "transcriptSummary" TEXT,
+    "publishedAt" TIMESTAMP(3) NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "YoutubeVideo_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "FactCheckSession" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "contentType" "ContentType" NOT NULL,
+    "contentId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "FactCheckSession_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Claim" (
+    "id" TEXT NOT NULL,
+    "factCheckSessionId" TEXT NOT NULL,
+    "index" INTEGER NOT NULL,
+    "content" TEXT NOT NULL,
+    "context" TEXT NOT NULL,
+    "detectionReason" TEXT NOT NULL,
+    "metadata" JSONB,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Claim_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ClaimVerification" (
+    "id" TEXT NOT NULL,
+    "factCheckSessionId" TEXT NOT NULL,
+    "claimId" TEXT NOT NULL,
+    "verdict" "VerdictType" NOT NULL,
+    "verdictReason" TEXT NOT NULL,
+    "evidences" JSONB NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "ClaimVerification_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_provider_providerSub_key" ON "User"("provider", "providerSub");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "YoutubeVideo_channelId_key" ON "YoutubeVideo"("channelId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "FactCheckSession_contentType_contentId_key" ON "FactCheckSession"("contentType", "contentId");
+
+-- AddForeignKey
+ALTER TABLE "FactCheckSession" ADD CONSTRAINT "FactCheckSession_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Claim" ADD CONSTRAINT "Claim_factCheckSessionId_fkey" FOREIGN KEY ("factCheckSessionId") REFERENCES "FactCheckSession"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ClaimVerification" ADD CONSTRAINT "ClaimVerification_factCheckSessionId_fkey" FOREIGN KEY ("factCheckSessionId") REFERENCES "FactCheckSession"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ClaimVerification" ADD CONSTRAINT "ClaimVerification_claimId_fkey" FOREIGN KEY ("claimId") REFERENCES "Claim"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
